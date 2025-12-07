d63f4f2c6b2fe5fac7fd08bbe59c25ede4367d83
Fabrice <schaubfabrice@gmail.com>
2025-12-07 23:21:59 +0100

    solving day 1 part 1

--- src/days/days-day1.adb
+++ src/days/days-day1.adb
@@ -1,11 +1,65 @@
 with Ada.Text_IO;
+with Utility;
 
 package body Days.Day1 is
    use Ada.Text_IO;
+   use Utility;
 
    procedure Solve (C : in out Day1_Challenge; Input : String) is
+      Start, Stop : Positive := Input'First;
+
+      Direction_Allowed : constant array (1 .. 2) of Character := ('R', 'L');
+      Direction_Offset  : constant Integer := 1;
+      Limit             : constant Integer := 100;
+
+      Zeroes  : Integer := 0;
+      Counter : Integer := 50;
+      Mode    : Character;
    begin
-      Put_Line ("Solving Day 1 with input: " & Input);
+      while Start <= Input'Last loop
+         while (Stop <= Input'Last) and then (Input (Stop) /= ASCII.LF) loop
+            Stop := Stop + 1;
+         end loop;
+
+         declare
+            Val     : Integer;
+            Line    : constant String := Input (Start .. Stop - 1);
+            Numbers : constant String :=
+              Line (Line'First + Direction_Offset .. Line'Last);
+         begin
+            if Line'Length = 0 then
+               goto Next;
+            end if;
+
+            Mode := Line (Line'First);
+            if (for all C of Direction_Allowed => C /= Mode) then
+               Put_Line ("Invalid mode: " & Mode);
+               return;
+            end if;
+
+            if not parse_int (Numbers, Val) then
+               Put_Line ("Invalid number: " & Numbers);
+               return;
+            end if;
+
+            if Mode = Direction_Allowed (1) then
+               Counter := (Counter + Val) mod Limit;
+            else
+               Counter := (Counter - Val) mod Limit;
+            end if;
+
+            if Counter = 0 then
+               Zeroes := Zeroes + 1;
+            end if;
+
+            <<Next>>
+         end;
+         Start := Stop + 1;
+         Stop := Start;
+      end loop;
+
+      Put_Line ("Final Counter: " & Integer'Image (Counter));
+      Put_Line ("Number of Zeroes: " & Integer'Image (Zeroes));
    end Solve;
 
 end Days.Day1;
